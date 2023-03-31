e04c70a2cb1646decfe43bd887d3e127a1025c31
Andreina Salcedo <ansalti27@gmail.com>
2023-03-31 19:24:38 -0300

    Primer commit

---
+++ mascotaApp/src/mascotaapp/MascotaApp.java
@@ -0,0 +1,20 @@
+
+package mascotaapp;
+
+import mascotaapp.entidades.Perro;
+
+/**
+ *
+ * @author ansal
+ */
+public class MascotaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Perro p1 = new Perro();
+       Perro p2 = new Perro();
+    }
+    
+}
---
+++ mascotaApp/src/mascotaapp/entidades/Perro.java
@@ -0,0 +1,14 @@
+
+package mascotaapp.entidades;
+
+/**
+ *
+ * @author ansal
+ */
+public class Perro {
+    public String nombre;
+     public String apodo;
+     public String raza;
+     public String tipo;      
+     public int edad;        
+}
